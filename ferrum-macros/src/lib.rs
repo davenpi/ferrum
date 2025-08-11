08d9eb61a886a1abb30d86d82b2a859e16b49f8f
davenpi <davenport.ianc@gmail.com>
2025-08-11 16:40:33 -0400

    feat: add macros crate

---
+++ ferrum-macros/src/lib.rs
@@ -0,0 +1,28 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, ItemFn, ReturnType};
+
+#[proc_macro_attribute]
+pub fn task(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let input_fn = parse_macro_input!(item as ItemFn);
+    
+    let fn_name = &input_fn.sig.ident;
+    let fn_inputs = &input_fn.sig.inputs;
+    let fn_body = &input_fn.block;
+    let fn_vis = &input_fn.vis;
+    
+    // Extract return type from async fn
+    let output_type = match &input_fn.sig.output {
+        ReturnType::Type(_, ty) => quote! { #ty },
+        ReturnType::Default => quote! { () },
+    };
+    
+    // Generate the new function that returns a TaskWrapper
+    let expanded = quote! {
+        #fn_vis fn #fn_name(#fn_inputs) -> impl ::ferrum::runtime::DistributedTask<Output = #output_type> {
+            ::ferrum::runtime::TaskWrapper::new(|| async move #fn_body)
+        }
+    };
+    
+    TokenStream::from(expanded)
+}
